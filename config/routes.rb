6de4380c69da51c29a6c127c4583e14762ed2790
gal1178 <splattakasvs16@gmail.com>
2018-04-04 15:19:19 +0300

    gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :toys
-  root "toys#main"
+  root "toys#index"
 
   get 'contakt', to: 'toys#contakt'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
