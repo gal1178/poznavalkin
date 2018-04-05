1af0a9b641ebe8e787c4111cb59b5c9b3d624778
gal1178 <splattakasvs16@gmail.com>
2018-04-05 11:59:06 +0300

    stable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :toys
-  root "toys#index"
+  root "toys#main"
 
   get 'contakt', to: 'toys#contakt'
+  get 'razr', to: 'toys#razr'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
